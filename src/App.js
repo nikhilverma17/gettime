505e35bfd1eb2c8c753e64381e057cfe868317ee
Nikhil Verma <63592210+nikhilverma17@users.noreply.github.com>
2023-08-05 17:24:45 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  var[now,setTime]=useState("Time")
+  function clock(){
+    let time = new Date().toLocaleTimeString({hour12: true });
+    setTime(time)
+    setInterval(clock,1000)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{now}</h1>
+      <button onClick={clock}>Click Here to get time</button>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+function Footer(){
+    return(
+        <footer>Copyright Nikhil Verma</footer>
+    )
+}
+export default Footer
